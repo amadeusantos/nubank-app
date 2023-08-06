29beb1e972dc257e3b51582177009f932c8c72b4
Amadeu Santos <amadeusantos31@gmail.com>
2023-08-06 18:53:48 -0300

    fix: separando a estilização do corpo do Service

--- src/components/service/index.tsx
+++ src/components/service/index.tsx
@@ -1,29 +1,21 @@
 import { Text, View } from "react-native";
 import Button from "../button";
-import { MaterialIcons } from '@expo/vector-icons';
+import { MaterialIcons } from "@expo/vector-icons";
+import { styles } from "./style";
 
 interface ServiceProps {
   name: string;
-  icon:  any;
+  icon: any;
 }
 
-export default function Service({name, icon}: ServiceProps) {
-    return (
-        <Button>
-          <View
-            style={{
-              flexDirection: "row",
-              justifyContent: "space-between",
-              borderRadius: 16,
-              backgroundColor: "#f5f5f5",
-              padding: 16,
-              alignItems: "center",
-            }}
-          >
-            <MaterialIcons name={icon} size={24} color="black" />
-            <Text>{name}</Text>
-            <Text style={{ fontSize: 12,backgroundColor: "#8A05BE", color: "#f5f5f5", paddingHorizontal: 2, borderRadius: 5}}>Novo</Text>
-          </View>
-        </Button>
-    )
+export default function Service({ name, icon }: ServiceProps) {
+  return (
+    <Button>
+      <View style={styles.container}>
+        <MaterialIcons name={icon} size={24} color="black" />
+        <Text>{name}</Text>
+        <Text style={styles.tag}>Novo</Text>
+      </View>
+    </Button>
+  );
 }
---
+++ src/components/service/style.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        borderRadius: 16,
+        backgroundColor: "#f5f5f5",
+        padding: 16,
+        alignItems: "center",
+    },
+    tag: { fontSize: 12, backgroundColor: "#8A05BE", color: "#f5f5f5", paddingHorizontal: 2, borderRadius: 5 }
+})
