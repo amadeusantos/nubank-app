20ae43095272310412535b38bf9eea6286ee2773
Amadeu Santos <amadeusantos31@gmail.com>
2023-08-06 18:54:04 -0300

    fix: separando a estilização do corpo do Services

--- src/components/services/index.tsx
+++ src/components/services/index.tsx
@@ -1,13 +1,14 @@
 import { FlatList, Text, View } from "react-native";
 import Service from "../service";
+import { styles } from "./style";
 
 export default function Services() {
-    return (
-        <View style={{ marginHorizontal: 16 }}>
-        <Text style={{fontSize: 17, fontWeight: "bold", marginBottom: 6}}>Acompanhe também</Text>
-        <Service name="Assistente de pagamentos" icon={"support-agent"} />
-        <Service name="Seguro vida" icon={"person-outline"} />
-        <Service name="seguro celular" icon={"smartphone"} />
-      </View>
-    )
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Acompanhe também</Text>
+      <Service name="Assistente de pagamentos" icon={"support-agent"} />
+      <Service name="Seguro vida" icon={"person-outline"} />
+      <Service name="seguro celular" icon={"smartphone"} />
+    </View>
+  );
 }
