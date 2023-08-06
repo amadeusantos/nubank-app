8a881691469df723836a66e5b60af838e77e6eb3
Amadeu Santos <amadeusantos31@gmail.com>
2023-08-06 18:54:17 -0300

    fix: separando a estilização do corpo do Card

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,5 +1,6 @@
 import { Image, Text, View } from "react-native";
 import Button from "../button";
+import { styles } from "./style";
 
 interface CardProps {
   title: string;
@@ -7,19 +8,17 @@ interface CardProps {
   image: string;
 }
 
-export default function Card({title, information, image}: CardProps) {
-    return (
+export default function Card({ title, information, image }: CardProps) {
+  return (
+    <Button>
+      <View style={styles.container}>
+        <Image style={styles.textTitle} source={{ uri: image }} />
+        <Text>{title}</Text>
+        <Text style={styles.textInformation}>{information}</Text>
         <Button>
-          <View  style={{width: 256, height: 200, borderRadius: 16, alignContent: "flex-start", backgroundColor: "#f5f5f5"}}>
-            <Image style={{width: 256, height: 94, borderTopLeftRadius: 16, borderTopRightRadius: 16}} source={{uri: image}} />
-            <Text>{title}</Text>
-            <Text style={{marginTop: 8, marginBottom: 4 }}>{information}</Text>
-            <Button>
-              <Text style={{ width: 70, fontSize: 12, backgroundColor: "#8A05BE", color: "#f5f5f5", padding: 5, borderRadius: 12}}>
-                Conhecer
-              </Text>
-            </Button>
-          </View>
+          <Text style={styles.buttonKnow}>Conhecer</Text>
         </Button>
-    )
+      </View>
+    </Button>
+  );
 }
---
+++ src/components/card/style.ts
@@ -0,0 +1,8 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: { width: 256, height: 200, borderRadius: 16, alignContent: "flex-start", backgroundColor: "#f5f5f5" },
+    textTitle: { width: 256, height: 94, borderTopLeftRadius: 16, borderTopRightRadius: 16 },
+    textInformation: { marginTop: 8, marginBottom: 4 },
+    buttonKnow: { width: 70, fontSize: 12, backgroundColor: "#8A05BE", color: "#f5f5f5", padding: 5, borderRadius: 12 }
+})
