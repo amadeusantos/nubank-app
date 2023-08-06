f0e6d5542bc900a359d7039f92f428a896e2b2cf
Amadeu Santos <amadeusantos31@gmail.com>
2023-08-06 18:51:25 -0300

    fix: separando a estilização do corpo do QuickAccess

--- src/components/quickAccess/index.tsx
+++ src/components/quickAccess/index.tsx
@@ -1,21 +1,21 @@
 import { Image, ImageSourcePropType, Text, View } from "react-native";
 import Button from "../button";
+import { styles } from "./style";
 
 interface Access {
-    name: string
-    icon: ImageSourcePropType
+  name: string;
+  icon: ImageSourcePropType;
 }
 
-
-export default function QuickAccess({name, icon}: Access) {
-    return (
-        <Button>
-            <View style={{justifyContent: "center", alignItems: "center", width: 68}}>
-            <View style={{justifyContent: "center", alignItems: "center", width: 64, height: 64, backgroundColor: "#F5F5F5", borderRadius: 30}}>
-            <Image source={icon} style={{width: 36, height: 36}} />
-            </View>
-            <Text>{name}</Text>
-            </View>
-        </Button>
-    )
+export default function QuickAccess({ name, icon }: Access) {
+  return (
+    <Button>
+      <View style={styles.container}>
+        <View style={styles.ellipse}>
+          <Image source={icon} style={styles.icon} />
+        </View>
+        <Text>{name}</Text>
+      </View>
+    </Button>
+  );
 }
---
+++ src/components/quickAccess/style.ts
@@ -0,0 +1,7 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: { justifyContent: "center", alignItems: "center", width: 68 },
+    ellipse: { justifyContent: "center", alignItems: "center", width: 64, height: 64, backgroundColor: "#F5F5F5", borderRadius: 30 },
+    icon: { width: 36, height: 36 }
+})
