57b70686891de41e101ac7be5de1b4f35d9a5829
Amadeu Santos <amadeusantos31@gmail.com>
2023-08-06 17:25:07 -0300

    feat: fazendo o Point

---
+++ src/components/point/index.tsx
@@ -0,0 +1,7 @@
+import { View } from "react-native";
+
+export default function Point() {
+    return (
+        <View style={{width: 30, height: 30, borderRadius: 30, backgroundColor: "#F5F5F5"}}/>
+    )
+}
