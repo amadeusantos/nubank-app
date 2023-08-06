4471de1e657f4d35f34fcce93fe5b50f26fc08e5
Amadeu Santos <amadeusantos31@gmail.com>
2023-08-06 18:53:28 -0300

    fix: separando a estilização do corpo do CreditCard

--- src/components/creditCard/index.tsx
+++ src/components/creditCard/index.tsx
@@ -1,6 +1,7 @@
 import { View, Text } from "react-native";
 import Button from "../button";
 import { AntDesign } from "@expo/vector-icons";
+import { styles } from "./style";
 
 interface CreditCardProps {
   value: string;
@@ -8,25 +9,23 @@ interface CreditCardProps {
   active?: boolean;
 }
 
-export default function CreditCard({value, limite, active}: CreditCardProps) {
-    return (
-        <Button>
-        <View
-          style={{
-            flexDirection: "row",
-            justifyContent: "space-between",
-            paddingHorizontal: 16
-          }}
-        >
-          <View>
-            <Text style={{fontSize: 18, fontWeight: "bold", marginBottom: 8}}>Cartão de crédito</Text>
-            <Text style={{fontSize: 18}}>Fatura atual:</Text>
-            <Text style={{fontSize: 18, marginBottom: 6}}>{value}</Text>
-            <Text style={{ fontSize: 12}}>limite disponivel de R$ {limite}</Text>
-            <Text style={{ fontSize: 12}}>Débito automatico {active? "ativado": "desativo"}</Text>
-          </View>
-          <AntDesign name="right" size={20} color="black" />
+export default function CreditCard({ value, limite, active }: CreditCardProps) {
+  return (
+    <Button>
+      <View style={styles.container}>
+        <View>
+          <Text style={styles.textTitle}>Cartão de crédito</Text>
+          <Text style={styles.textSubTitle}>Fatura atual:</Text>
+          <Text style={styles.textValue}>{value}</Text>
+          <Text style={styles.textSubInfo}>
+            limite disponivel de R$ {limite}
+          </Text>
+          <Text style={styles.textSubInfo}>
+            Débito automatico {active ? "ativado" : "desativo"}
+          </Text>
         </View>
-      </Button>
-    )
+        <AntDesign name="right" size={20} color="black" />
+      </View>
+    </Button>
+  );
 }
---
+++ src/components/creditCard/style.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        paddingHorizontal: 16
+    },
+    textTitle: { fontSize: 18, fontWeight: "bold", marginBottom: 8 },
+    textSubTitle: { fontSize: 18 },
+    textValue: { fontSize: 18, marginBottom: 6 },
+    textSubInfo: { fontSize: 12 }
+})
