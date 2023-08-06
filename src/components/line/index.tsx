cee7e12122c6b0625a64678c59ee5fe05fdd91de
Amadeu Santos <amadeusantos31@gmail.com>
2023-08-06 18:53:05 -0300

    fix: separando a estilização do corpo do Line

--- src/components/line/index.tsx
+++ src/components/line/index.tsx
@@ -1,7 +1,6 @@
 import { View } from "react-native";
+import { styles } from "./style";
 
 export default function Line() {
-    return (
-        <View style={{backgroundColor: "#F5F5F5", padding: 1, marginVertical: 8}} />
-    )
+  return <View style={styles.line} />;
 }
---
+++ src/components/line/style.ts
@@ -0,0 +1,3 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({ line: { backgroundColor: "#F5F5F5", padding: 1, marginVertical: 8 } })
