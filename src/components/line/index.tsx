9a1964f9cdea2fb0d86596ce0eb3a3474a801dce
Amadeu Santos <amadeusantos31@gmail.com>
2023-08-06 17:27:48 -0300

    feat: fazendo o Line

---
+++ src/components/line/index.tsx
@@ -0,0 +1,7 @@
+import { View } from "react-native";
+
+export default function Line() {
+    return (
+        <View style={{backgroundColor: "#F5F5F5", padding: 1, marginVertical: 8}} />
+    )
+}
